10b62559c0ff7d0ba7a9d4520a81a7bc5c702cfd
kaneko <kaneko@e2info.com>
2019-02-17 14:59:10 +0900

    新規追加

---
+++ src/app/Http/Controllers/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Shared\File;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $spreadsheet = IOFactory::load(public_path() . '/excel/template.xlsx');
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A3', 'サンプル株式会社');
+
+        File::setUseUploadTempDirectory(public_path());
+
+        $writer = IOFactory::createWriter($spreadsheet, 'Tcpdf');
+        $writer->setFont('ipaexm');    //日本語文字化け対策
+        $writer->save(public_path() . '/excel/output.pdf');
+
+        $writer = new Xlsx($spreadsheet);
+        $writer->save(public_path() . '/excel/output.xlsx');
+    }
+}
